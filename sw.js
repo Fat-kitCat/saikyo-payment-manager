3ccd3dde075edd1326f6a92fc4cc97cf388be73d
Fat-kitCat <runa.4.22s2@gmail.com>
2026-09-11 03:51:26 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
-const CACHE='saikyo-payment-manager-v2';
+const CACHE='saikyo-payment-manager-v3';
 self.addEventListener('install',e=>{
+ self.skipWaiting();
  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json'])));
 });
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
